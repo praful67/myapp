6a449a76ae119e46f890cf8eae658621ffa108d1
Praful <2017UEE0067@iitjammu.ac.in>
2019-12-13 21:19:20 +0530

    Updates

---
+++ precache-manifest.73226eb00a8a3d00318f3ebde1d56b45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c481e1a59c901289fd1c6e86fcd7bd33",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "2acbd7ac1d47a6e30cb0",
+    "url": "/myapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c88cebed7dec8a545eeb",
+    "url": "/myapp/static/js/2.250dc4af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myapp/static/js/2.250dc4af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2acbd7ac1d47a6e30cb0",
+    "url": "/myapp/static/js/main.aa5099c9.chunk.js"
+  },
+  {
+    "revision": "085eaedc6753e841dd23",
+    "url": "/myapp/static/js/runtime-main.2b4fa720.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
